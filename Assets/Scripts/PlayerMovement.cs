a02f110d4aebbfd5034fb66f99c1db91c5e3e360
DimenDie <dimarepalo3@gmail.com>
2023-05-09 06:21:37 +0300

    movement done

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,78 +1,225 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    private MovementCollision movementCollision;
+    private MovementCollision coll;
+    [HideInInspector]
+    public Rigidbody2D rb;
 
-    [Header("Move Settings")]
-    public float speed = 10f;
-    public float grabMoveSpeed = 5f;
-    public float slideSpeed = 3f;
-    public float jumpForce = 5f;
+    [Space]
+    [Header("Stats")]
+    public float speed = 10;
+    public float jumpForce = 50;
+    public float slideSpeed = 5;
+    public float wallJumpLerp = 10;
+    public float dashSpeed = 20;
 
-    public int side = 1; //Animations shit, later
-
+    [Space]
+    [Header("Booleans")]
     public bool canMove;
-    bool wallGrab;
-    bool wallJumped;
+    public bool wallGrab;
+    public bool wallJumped;
+    public bool wallSlide;
+    public bool isDashing;
 
-    private void Start()
+    [Space]
+
+    private bool groundTouch;
+    private bool hasDashed;
+
+    public int side = 1;
+
+    //[Space]
+    //[Header("Polish")]
+    //public ParticleSystem dashParticle;
+    //public ParticleSystem jumpParticle;
+    //public ParticleSystem wallJumpParticle;
+    //public ParticleSystem slideParticle;
+
+    // Start is called before the first frame update
+    void Start()
     {
+        coll = GetComponent<MovementCollision>();
         rb = GetComponent<Rigidbody2D>();
-        movementCollision = GetComponent<MovementCollision>();
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
+        float xRaw = Input.GetAxisRaw("Horizontal");
+        float yRaw = Input.GetAxisRaw("Vertical");
         Vector2 dir = new Vector2(x, y);
 
         Walk(dir);
 
-        wallGrab = movementCollision.onWall && Input.GetKey(KeyCode.LeftShift);
-        print(rb.velocity);
-        if (wallGrab)
+        if (coll.onWall && Input.GetButton("Fire3") && canMove)
+        {
+            wallGrab = true;
+            wallSlide = false;
+        }
+
+        if (Input.GetButtonUp("Fire3") || !coll.onWall || !canMove)
+        {
+            wallGrab = false;
+            wallSlide = false;
+        }
+
+        if (coll.onGround && !isDashing)
+        {
+            wallJumped = false;
+            GetComponent<Jumping>().enabled = true;
+        }
+
+        if (wallGrab && !isDashing)
         {
             rb.gravityScale = 0;
-            rb.velocity = new Vector2(rb.velocity.x, y * speed);
+            if (x > .2f || x < -.2f)
+                rb.velocity = new Vector2(rb.velocity.x, 0);
+
+            float speedModifier = y > 0 ? .5f : 1;
+
+            rb.velocity = new Vector2(rb.velocity.x, y * (speed * speedModifier));
         }
         else
         {
-            rb.gravityScale = 1;
+            rb.gravityScale = 3;
         }
 
+        if (coll.onWall && !coll.onGround)
+        {
+            if (x != 0 && !wallGrab)
+            {
+                wallSlide = true;
+                WallSlide();
+            }
+        }
 
-        if(Input.GetButtonDown("Jump"))
+        if (!coll.onWall || coll.onGround)
+            wallSlide = false;
+
+        if (Input.GetButtonDown("Jump"))
         {
 
-            if(movementCollision.onGround)
-            {
+            if (coll.onGround)
                 Jump(Vector2.up, false);
-            }
-            else
-            {
+            if (coll.onWall && !coll.onGround)
                 WallJump();
-            }
         }
 
-        if(movementCollision.onWall && !movementCollision.onGround && !Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetButtonDown("Fire1") && !hasDashed)
         {
-            WallSlide();
+            if (xRaw != 0 || yRaw != 0)
+                Dash(xRaw, yRaw);
         }
 
-        if(x > 0) 
+        if (coll.onGround && !groundTouch)
+        {
+            GroundTouch();
+            groundTouch = true;
+        }
+
+        if (!coll.onGround && groundTouch)
+        {
+            groundTouch = false;
+        }
+
+        WallParticle(y);
+
+        if (wallGrab || wallSlide || !canMove)
+            return;
+
+        if (x > 0)
         {
             side = 1;
         }
-        if(x < 0)
+        if (x < 0)
         {
             side = -1;
         }
+
+
+    }
+
+    void GroundTouch()
+    {
+        hasDashed = false;
+        isDashing = false;
+    }
+
+    private void Dash(float x, float y)
+    {
+        hasDashed = true;
+
+
+        rb.velocity = Vector2.zero;
+        Vector2 dir = new Vector2(x, y);
+
+        rb.velocity += dir.normalized * dashSpeed;
+        StartCoroutine(DashWait());
+    }
+
+    IEnumerator DashWait()
+    {
+        StartCoroutine(GroundDash());
+
+        //dashParticle.Play();
+        rb.gravityScale = 0;
+        GetComponent<Jumping>().enabled = false;
+        wallJumped = true;
+        isDashing = true;
+
+        yield return new WaitForSeconds(.3f);
+
+        //dashParticle.Stop();
+        rb.gravityScale = 3;
+        GetComponent<Jumping>().enabled = true;
+        wallJumped = false;
+        isDashing = false;
+    }
+
+    IEnumerator GroundDash()
+    {
+        yield return new WaitForSeconds(.15f);
+        if (coll.onGround)
+            hasDashed = false;
+    }
+
+    private void WallJump()
+    {
+        if ((side == 1 && coll.onRightWall) || side == -1 && !coll.onRightWall)
+        {
+            side *= -1;
+        }
+
+        StopCoroutine(DisableMovement(0));
+        StartCoroutine(DisableMovement(.1f));
+
+        Vector2 wallDir = coll.onRightWall ? Vector2.left : Vector2.right;
+
+        Jump((Vector2.up / 1.5f + wallDir / 1.5f), true);
+
+        wallJumped = true;
+    }
+
+    private void WallSlide()
+    {
+
+        if (!canMove)
+            return;
+
+        bool pushingWall = false;
+        if ((rb.velocity.x > 0 && coll.onRightWall) || (rb.velocity.x < 0 && coll.onLeftWall))
+        {
+            pushingWall = true;
+        }
+        float push = pushingWall ? 0 : rb.velocity.x;
+
+        rb.velocity = new Vector2(push, -slideSpeed);
     }
 
     private void Walk(Vector2 dir)
@@ -83,20 +230,25 @@ public class PlayerMovement : MonoBehaviour
         if (wallGrab)
             return;
 
-
-        rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
+        if (!wallJumped)
+        {
+            rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = Vector2.Lerp(rb.velocity, (new Vector2(dir.x * speed, rb.velocity.y)), wallJumpLerp * Time.deltaTime);
+        }
     }
 
-    private void WallJump()
+    private void Jump(Vector2 dir, bool wall)
     {
-        StopCoroutine(DisableMovement(0));
-        StartCoroutine(DisableMovement(.1f));
+        //slideParticle.transform.parent.localScale = new Vector3(ParticleSide(), 1, 1);
+        //ParticleSystem particle = wall ? wallJumpParticle : jumpParticle;
 
-        Vector2 wallDir = movementCollision.onRightWall ? Vector2.left : Vector2.right;
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.velocity += dir * jumpForce;
 
-        Jump((Vector2.up * 1.5f + wallDir * 1.5f), true);
-
-        wallJumped = true;
+        //particle.Play();
     }
 
     IEnumerator DisableMovement(float time)
@@ -105,14 +257,30 @@ public class PlayerMovement : MonoBehaviour
         yield return new WaitForSeconds(time);
         canMove = true;
     }
-    private void Jump(Vector2 dir, bool wall)
+
+    void RigidbodyDrag(float x)
     {
-        rb.velocity = new Vector2(rb.velocity.x, 0);
-        rb.velocity += dir * jumpForce;
+        rb.drag = x;
     }
-    
-    private void WallSlide()
+
+    void WallParticle(float vertical)
     {
-        rb.velocity = new Vector2(rb.velocity.x, -slideSpeed);
+        //var main = slideParticle.main;
+
+        if (wallSlide || (wallGrab && vertical < 0))
+        {
+            //slideParticle.transform.parent.localScale = new Vector3(ParticleSide(), 1, 1);
+            //main.startColor = Color.white;
+        }
+        else
+        {
+            //main.startColor = Color.clear;
+        }
+    }
+
+    int ParticleSide()
+    {
+        int particleSide = coll.onRightWall ? 1 : -1;
+        return particleSide;
     }
 }
